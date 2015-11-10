c4592f74e87ebd4dabad0043ff9a9de94121cef7
Zoe <dadac123@gmail.com>
2015-11-10 22:12:59 +0800

    delete writing and reading pages

--- diary-web.py
+++ diary-web.py
@@ -1,30 +1,22 @@
-
+# -*- coding: utf-8 -*-
 # A very simple Bottle Hello World app for you to get started with...
 from bottle import default_app
-
-# @route('/')
-# def hello_world():
-#    return 'Hello from Bottle!'
-
-# -*- coding: utf-8 -*-
 from bottle import Bottle,route,run,debug,request,template
 
 from datetime import datetime
 import sys
 
 
-
-@route('/')
-@route('/diary')
-def greeting():
+@route('/',method='GET')
+def diary():
     diaryFile = open('diary.txt')
     diaryContent = diaryFile.read()
     diaryFile.close()
     diaryContent=diaryContent.replace('\n', '<br />')
-    return '''
+    output ='''
     <head>
   <meta charset="utf-8">
-  <title>Zoe's Diary</title>
+  <title>Diary of Yours</title>
   <!-- Bootstrap core CSS -->
   <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom styles for this template -->
@@ -33,36 +25,39 @@ def greeting():
 <body>
 
     <h3 class="masthead-brand">Diary of Yours</h3>
-    <p>Hello, I am your diary.
+    <p>Hello, I am your diary.你好
       <br />Do you wanna share something with me?</p>
-    <p class="lead">
-      <a href="/writing" class="btn btn-lg btn-default">Writing diary</a>
-      <a href="/reading" class="btn btn-lg btn-default">Reading diary</a>
-    </p>
 
-    <p>Share your feeling:</p>
+    <p><b>Share your feeling:</b></p>
 <form action="/writing" method="GET">
 <input type="text" size="70" maxlength="100" name="newdiary">
 <input type="submit" name="save" value="Save">
 </form>
 
-<p>====Diary====</p>'''+diaryContent+'''<br /><br /><a href='/writing'>Writing Diary<a><br /><a href='/'>Back Home<a>
+<p>====Diary====</p>'''+diaryContent+'''<br /><br />
 
   <div class="inner">
-    <p>life is wonderful</p>
+    <p><i>life is wonderful</i></p>
   </div>
 
 </body>'''
+    if request.GET.get('save','').strip():
 
+        today=datetime.now()
+        newDiary=request.GET.get('newdiary', '').strip()
 
-@route('/reading')
-def reading():
-    diaryFile = open('diary.txt')
-    diaryContent = diaryFile.read()
-    diaryFile.close()
-    diaryContent=diaryContent.replace('\n', '<br />')
-    output='<p>====Diary====</p>'+diaryContent+"<br /><br /><a href='/writing'>Writing Diary<a><br /><a href='/'>Back Home<a>"
+        diaryFile = open('diary.txt','a')
+        diaryFile.write('\n'+today.strftime("%y/%m/%d")+ ' '+newDiary)
+        diaryFile.close()
+
+        diaryFile = open('diary.txt')
+        diaryContent = diaryFile.read()
+        diaryFile.close()
+        diaryContent=diaryContent.replace('\n', '<br />')
+
+        return output
     return output
+
 
 @route('/writing',method='GET')
 def writing():
@@ -80,12 +75,6 @@ def writing():
         diaryFile.close()
         diaryContent=diaryContent.replace('\n', '<br />')
 
-#        return '''
-#<p>The new diary was saved.</p>
-#<a href='/writing'>Writing Diary<a>
-#<a href='/reading'>Reading Diary<a>
-#<a href='/'>Back Home<a>
-#'''
 
         return '''
   <head>
@@ -129,7 +118,7 @@ def writing():
 <a href='/'>Back Home<a>
 '''
 
-# run(diary,host='localhost', port=1234, debug=True)'''
-
 
 application = default_app()
+
+run(host='localhost', port=1234, debug=True)
