e9b58a91367cff74358fa36e711367c402e8454a
Zoe <dadac123@gmail.com>
2015-11-11 21:35:50 +0800

    add introduction

--- diary-web.py
+++ diary-web.py
@@ -1,57 +1,60 @@
 # -*- coding: utf-8 -*-
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app
-from bottle import Bottle,route,run,debug,request,template
+from bottle import default_app,Bottle,route,run,debug,request,template
 
 from datetime import datetime
 import sys
 
-
-@route('/hello/<name>')
-def greet(name='Stranger'):
-    return template('Hello {{name}}, how are you?', name=name)
-
 @route('/',method='GET')
 @route('/<user_name>')
-def diary(user_name='guest'):
-#    print user_name
+def diary(user_name='小小游侠'):
+
     diaryFile = open('diary.txt')
     diaryContent = diaryFile.read()
     diaryFile.close()
     diaryContent=diaryContent.replace('\n', '<br />')
-    lujing="'/'+username"
+
     output_start ='''
-   <head>
-  <meta charset="utf-8">
-  <title>Diary of Yours</title>
-  <!-- Bootstrap core CSS -->
-  <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom styles for this template -->
-  <link href="css/cover.css" rel="stylesheet">
+<head>
+<meta charset="utf-8">
+<title>Diary of Yours</title>
+<!-- Bootstrap core CSS -->
+<link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="css/cover.css" rel="stylesheet">
 </head>
 <body>
 
-    <h3 class="masthead-brand">Diary of Yours</h3>
-    <p>Hello, I am your diary.你好
-      <br />Do you wanna share something with me?</p>
+<h1>小小游侠的聊天室</h1>
+<p>欢迎小小游侠来到聊天室小憩一会。
+<br />如果想用自己的名字加入交谈，
+<br />可以用 “zoejane.pythonanywhere.com/YOURNAME”进行登录，
+<br />名字可以是字母和数字和下划线的组合。
+<br />
+<br />想搭建一个相似的网页，可以看这里的教程。
+<br />这个网页的具体的代码也可以在这里看到。
+<br />
+<br />我是<a href='mailto:dadac123@gmail.com'>Zoe Jane</a>，欢迎一起分享你的心得和感悟。
+<br />我的<a href='mailto:dadac123@gmail.com'>Email</a>
+<br />我的<a href='http://zoejane.github.com'>GitHub</a>
+<br />我的<a href='http://zoejane.github.com'>GitBook</a>
+<br /></p>
 
-    <p><b>Share your feeling:</b></p>
+<p><b>Share your feeling:</b></p>
 <form action="'''
+
     output_middle='''" method="GET">
 <input type="text" size="70" maxlength="100" name="newdiary">
 <input type="submit" name="save_diary" value="Save">
 </form>
-
 <p>====Diary====</p>'''
     
     output_end='''<br /><br />
-
-  <div class="inner">
-    <p><i>life is wonderful</i></p>
-  </div>
-
+<div class="inner">
+<p><i>life is wonderful</i></p>
+</div>
 </body>'''
-    print user_name
+
     if request.GET.get('save_diary','').strip():
 
         today=datetime.now()
@@ -60,10 +63,6 @@ def diary(user_name='guest'):
         diaryFile = open('diary.txt')
         diaryContent = diaryFile.read()
         diaryFile.close()
-
-#        diaryFile = open('diary.txt','a')
-#        diaryFile.write('\n'+today.strftime("%y/%m/%d")+ '  ['+user_name+'] '+newDiary)
-#        diaryFile.close()
 
         with open('diary.txt', 'r+') as f:
             content = f.read()
